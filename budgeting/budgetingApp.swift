a6b616c1e62ef17a768d3fdf61a0e611cae96281
Kendall Easterly <qfletcher89@gmail.com>
2023-06-10 20:02:59 -0500

    Initial Commit

---
+++ budgeting/budgetingApp.swift
@@ -0,0 +1,17 @@
+//
+//  budgetingApp.swift
+//  budgeting
+//
+//  Created by Kendall Easterly on 6/10/23.
+//
+
+import SwiftUI
+
+@main
+struct budgetingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
